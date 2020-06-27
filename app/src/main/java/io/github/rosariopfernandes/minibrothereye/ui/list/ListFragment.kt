6486700bd173dc78051be9de9ceaf778b5fcf489
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-06-28 01:34:30 +0200

    refactor: use databinding instead of viewbinding

--- app/src/main/java/io/github/rosariopfernandes/minibrothereye/ui/list/ListFragment.kt
+++ app/src/main/java/io/github/rosariopfernandes/minibrothereye/ui/list/ListFragment.kt
@@ -6,12 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.view.isGone
-import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.paging.LoadState
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import io.github.rosariopfernandes.minibrothereye.databinding.FragmentListBinding
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -38,43 +34,17 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val charactersAdapter = CharacterAdapter { item ->
             Toast.makeText(context, "${item.name} doesnt have a profile yet",
                 Toast.LENGTH_SHORT).show()
-        }
+        }.apply { addLoadStateListener { binding.loadState = it.refresh } }
 
-        with (binding) {
-            rvCharacters.layoutManager =
-                StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-            rvCharacters.adapter = charactersAdapter
-        }
+        binding.characterAdapter = charactersAdapter
 
         lifecycleScope.launch {
             viewModel.flow.collectLatest { pagingData ->
                 charactersAdapter.submitData(pagingData)
-            }
-        }
-
-        charactersAdapter.addLoadStateListener { loadState ->
-            with (binding) {
-                when (loadState.refresh) {
-                    is LoadState.Loading -> {
-                        progressBar.isVisible = true
-                        rvCharacters.isGone = true
-                        tvError.isGone = true
-                    }
-                    is LoadState.Error -> {
-                        tvError.text = (loadState.refresh as LoadState.Error).error.message
-                        rvCharacters.isGone = true
-                        progressBar.isGone = true
-                        tvError.isVisible = true
-                    }
-                    else -> {
-                        rvCharacters.isVisible = true
-                        progressBar.isGone = true
-                        tvError.isGone = true
-                    }
-                }
             }
         }
     }
---
+++ app/src/main/java/io/github/rosariopfernandes/minibrothereye/util/DataBinding.kt
@@ -0,0 +1,44 @@
+package io.github.rosariopfernandes.minibrothereye.util
+
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.RecyclerView
+
+@BindingAdapter("adapter")
+fun bindRecyclerViewAdapter(view: RecyclerView, adapter: RecyclerView.Adapter<*>) {
+    view.adapter = adapter
+}
+
+@BindingAdapter("visibility")
+fun bindRecyclerViewVisibility(recyclerView: RecyclerView, state: LoadState) {
+    when (state) {
+        is LoadState.Loading -> recyclerView.isGone = true
+        is LoadState.Error -> recyclerView.isGone = true
+        else -> recyclerView.isVisible = true
+    }
+}
+
+@BindingAdapter("visibility")
+fun bindProgressBarVisibility(progressBar: ProgressBar, state: LoadState) {
+    when (state) {
+        is LoadState.Loading -> progressBar.isVisible = true
+        is LoadState.Error -> progressBar.isGone = true
+        else -> progressBar.isGone = true
+    }
+}
+
+@BindingAdapter("visibility")
+fun bindErrorTextViewVisibility(tvError: TextView, state: LoadState) {
+    when (state) {
+        is LoadState.Loading -> tvError.isGone = true
+        is LoadState.Error -> {
+            tvError.text = (state/* as LoadState.Error*/).error.message
+            tvError.isVisible = true
+        }
+        else -> tvError.isGone = true
+    }
+}
