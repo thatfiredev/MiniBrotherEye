2d592ca723f57b3c32b4c267dee491d818093d21
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-06-30 23:49:18 +0200

    refactor: migrate build scripts to kotlin (#10)
    * refactor: migrate build.gradle and settings.gradle to kotlin
    
    * refactor: app/build.gradle --> app/build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    extra.apply {
+        set("kotlin_version", "1.3.72")
+        set("hilt_version", "2.28-alpha")
+    }
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        val kotlinVersion = project.extra["kotlin_version"] as String
+        val hiltVersion = project.extra["hilt_version"] as String
+        classpath("com.android.tools.build:gradle:4.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
